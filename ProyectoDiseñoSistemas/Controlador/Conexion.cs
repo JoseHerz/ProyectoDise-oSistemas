32d68dd06ac87e24b28b0778b90150b898c11b31
JoseHerz <102639984+JoseHerz@users.noreply.github.com>
2023-07-28 14:54:23 -0600

    Creacion de la clase "Conexion"

---
+++ ProyectoDiseñoSistemas/Controlador/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ProyectoDiseñoSistemas.Controlador
+{
+    internal class Conexion
+    {
+        public SqlConnection GetConexion()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["DBPROYECTO"].ToString());
+        }
+    }
+}
