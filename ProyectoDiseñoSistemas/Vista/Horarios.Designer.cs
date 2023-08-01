9892407a984b59ea40935a011bb61ce249028c83
JoseHerz <102639984+JoseHerz@users.noreply.github.com>
2023-07-31 21:54:08 -0600

    Lista Buscar

--- ProyectoDiseñoSistemas/Vista/Horarios.Designer.cs
+++ ProyectoDiseñoSistemas/Vista/Horarios.Designer.cs
@@ -46,24 +46,24 @@
             this.button1 = new System.Windows.Forms.Button();
             this.MenuVertical = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.BTNGuardar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
             this.CMBHorario = new System.Windows.Forms.ComboBox();
-            this.TXTBuscar = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.BtnBuscar = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.BtnExportar = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.label4 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.BTNGuardar = new System.Windows.Forms.Button();
+            this.Buscar = new System.Windows.Forms.ListBox();
             this.BarraTitulo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.BtnResiz)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BtnMin)).BeginInit();
@@ -324,6 +324,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.panel2.Controls.Add(this.Buscar);
             this.panel2.Controls.Add(this.BTNGuardar);
             this.panel2.Controls.Add(this.comboBox1);
             this.panel2.Controls.Add(this.label9);
@@ -336,7 +337,6 @@
             this.panel2.Controls.Add(this.dateTimePicker1);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.CMBHorario);
-            this.panel2.Controls.Add(this.TXTBuscar);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.BtnBuscar);
             this.panel2.Controls.Add(this.label6);
@@ -350,29 +350,127 @@
             this.panel2.Size = new System.Drawing.Size(1007, 806);
             this.panel2.TabIndex = 38;
             // 
+            // BTNGuardar
+            // 
+            this.BTNGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNGuardar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BTNGuardar.Location = new System.Drawing.Point(598, 677);
+            this.BTNGuardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTNGuardar.Name = "BTNGuardar";
+            this.BTNGuardar.Size = new System.Drawing.Size(179, 49);
+            this.BTNGuardar.TabIndex = 56;
+            this.BTNGuardar.Text = "Guardar";
+            this.BTNGuardar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(783, 130);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 24);
+            this.comboBox1.TabIndex = 55;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(671, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 29);
+            this.label9.TabIndex = 54;
+            this.label9.Text = "Jornada:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(173, 202);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.TabIndex = 53;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 242);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 52;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(550, 238);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 29);
+            this.label8.TabIndex = 51;
+            this.label8.Text = "Final";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(86, 235);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 29);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Salida";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(67, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 29);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Llegada";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(639, 243);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(295, 22);
+            this.dateTimePicker2.TabIndex = 48;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(639, 200);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(295, 22);
+            this.dateTimePicker1.TabIndex = 47;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(545, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 29);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Inicio";
+            // 
             // CMBHorario
             // 
             this.CMBHorario.FormattingEnabled = true;
             this.CMBHorario.Location = new System.Drawing.Point(173, 130);
             this.CMBHorario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CMBHorario.Name = "CMBHorario";
-            this.CMBHorario.Size = new System.Drawing.Size(151, 24);
+            this.CMBHorario.Size = new System.Drawing.Size(170, 24);
             this.CMBHorario.TabIndex = 45;
-            // 
-            // TXTBuscar
-            // 
-            this.TXTBuscar.Location = new System.Drawing.Point(287, 64);
-            this.TXTBuscar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.TXTBuscar.Name = "TXTBuscar";
-            this.TXTBuscar.Size = new System.Drawing.Size(489, 22);
-            this.TXTBuscar.TabIndex = 44;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(54, 57);
+            this.label3.Location = new System.Drawing.Point(22, 58);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(218, 29);
             this.label3.TabIndex = 43;
@@ -425,111 +523,15 @@
             this.dataGridView2.TabIndex = 0;
             this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
-            // label4
+            // Buscar
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(545, 195);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 29);
-            this.label4.TabIndex = 46;
-            this.label4.Text = "Inicio";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(639, 200);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(295, 22);
-            this.dateTimePicker1.TabIndex = 47;
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(639, 243);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(295, 22);
-            this.dateTimePicker2.TabIndex = 48;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label5.Location = new System.Drawing.Point(67, 195);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 29);
-            this.label5.TabIndex = 49;
-            this.label5.Text = "Llegada";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(86, 235);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(81, 29);
-            this.label7.TabIndex = 50;
-            this.label7.Text = "Salida";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label8.Location = new System.Drawing.Point(550, 238);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(66, 29);
-            this.label8.TabIndex = 51;
-            this.label8.Text = "Final";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(173, 242);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 22);
-            this.textBox1.TabIndex = 52;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(173, 202);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(161, 22);
-            this.textBox2.TabIndex = 53;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(783, 130);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(151, 24);
-            this.comboBox1.TabIndex = 55;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label9.Location = new System.Drawing.Point(671, 125);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(106, 29);
-            this.label9.TabIndex = 54;
-            this.label9.Text = "Jornada:";
-            // 
-            // BTNGuardar
-            // 
-            this.BTNGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTNGuardar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.BTNGuardar.Location = new System.Drawing.Point(598, 677);
-            this.BTNGuardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BTNGuardar.Name = "BTNGuardar";
-            this.BTNGuardar.Size = new System.Drawing.Size(179, 49);
-            this.BTNGuardar.TabIndex = 56;
-            this.BTNGuardar.Text = "Guardar";
-            this.BTNGuardar.UseVisualStyleBackColor = true;
+            this.Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscar.FormattingEnabled = true;
+            this.Buscar.ItemHeight = 29;
+            this.Buscar.Location = new System.Drawing.Point(246, 54);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(508, 33);
+            this.Buscar.TabIndex = 57;
             // 
             // FrmHorarios
             // 
@@ -577,7 +579,6 @@
         private System.Windows.Forms.Panel MenuVertical;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox CMBHorario;
-        private System.Windows.Forms.TextBox TXTBuscar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button BtnBuscar;
         private System.Windows.Forms.Label label6;
@@ -594,5 +595,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button BTNGuardar;
+        private System.Windows.Forms.ListBox Buscar;
     }
 }
